00a725fc7f02b356421526ccd0df156b4502fd65
raktimproloy <raktimproloy@gmail.com>
2025-09-08 10:55:15 +0600

    update blog and module

--- src/app/modules/blog/blog.route.ts
+++ src/app/modules/blog/blog.route.ts
@@ -2,20 +2,23 @@ import express from "express";
 import { blogController } from "./blog.controller";
 import validateRequest from "../../middleware/validateRequest";
 import { BlogValidation } from "./blog.validation";
+import { adminAuth } from "../../middleware/adminAuth";
 
 const router = express.Router();
 
 router.post(
   "/create",
+  adminAuth(),
   validateRequest(BlogValidation.createBlogValidationSchema),
   blogController.createBlog
 );
 
 router.get("/", blogController.getAllBlogs);
 router.get("/:id", blogController.getSingleBlog);
-router.delete("/:id", blogController.deleteBlog);
+router.delete("/:id", adminAuth(), blogController.deleteBlog);
 router.put(
   "/:id",
+  adminAuth(),
   validateRequest(BlogValidation.updateBlogValidationSchema),
   blogController.updateBlog
 );
--- src/app/modules/module/module.route.ts
+++ src/app/modules/module/module.route.ts
@@ -3,20 +3,23 @@ import express from "express";
 import validateRequest from "../../middleware/validateRequest";
 import { ModuleValidation } from "./module.validation";
 import { moduleController } from "./module.controller";
+import { adminAuth } from "../../middleware/adminAuth";
 
 const router = express.Router();
 
 router.post(
   "/create",
+  adminAuth(),
   validateRequest(ModuleValidation.createModuleValidationSchema),
   moduleController.createModule
 );
 
 router.get("/", moduleController.getAllModules);
 router.get("/:id", moduleController.getSingleModule);
-router.delete("/:id", moduleController.deleteModule);
+router.delete("/:id", adminAuth(), moduleController.deleteModule);
 router.put(
   "/:id",
+  adminAuth(),
   validateRequest(ModuleValidation.updateModuleValidationSchema),
   moduleController.updateModule
 );
